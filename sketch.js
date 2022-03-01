d475ca53da150647c59baea7ad5a9b32b58c86bb
coderFeedForwardAlg <76785692+coderFeedForwardAlg@users.noreply.github.com>
2022-03-01 10:19:52 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
+
+let button, button2;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255,255,255);
+  button = createButton('download');
+  button.position(20,windowHeight - 20);
+  button.mousePressed(downLoad);
+  
+  button2 = createButton('clear');
+  button2.position(100,windowHeight - 20);
+  button2.mousePressed(clearr);
   
 }
 let oldX; 
@@ -12,7 +22,23 @@ function draw() {
   if(mouseIsPressed){
     line(pmouseX, pmouseY, mouseX, mouseY);
   }
+  
+  
+  
+  
+  //noLoop()
+    //save("myImage.png");
+    
+  
  
   
+}
+
+function downLoad(){
+  save("myImage.png");
+}
+
+function clearr(){
+  background(255,255,255);
 }
 
